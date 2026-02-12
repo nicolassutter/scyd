54ce874ab739aa0a93ceed350157fcffb8e52cdb
nicolassutter <contact.sutter.nicolas@gmail.com>
2026-02-12 19:41:35 +0100

    fix: head genereate for spa mode

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -60,4 +60,28 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      title: "Scyd - Self hosted music downloader",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Scyd is a simple and automated music downloader built with Nuxt 3, Go and open-source projects.",
+        },
+        { name: "theme-color", content: "#1e293b" },
+      ],
+      link: [
+        { rel: "icon", href: "/favicon.ico" },
+        // make sure the manifest is included even in spa mode
+        { rel: "manifest", href: "/manifest.webmanifest" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon-180x180.png",
+        },
+        { rel: "mask-icon", href: "/maskable-icon-512x512.svg", color: "#FFFFFF" },
+      ],
+    }
+  }
 });
