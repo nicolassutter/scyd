4305b0cff740e4bcc598fe0052573d9b515dcca4
nicolassutter <contact.sutter.nicolas@gmail.com>
2026-08-24 08:54:25 +0200

    chore: fix typo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -67,7 +67,7 @@ export default defineNuxtConfig({
         {
           name: "description",
           content:
-            "Scyd is a simple and automated music downloader built with Nuxt 3, Go and open-source projects.",
+            "Scyd is a simple and automated music downloader built with Nuxt 4, Go and open-source projects.",
         },
         { name: "theme-color", content: "#1e293b" },
       ],
@@ -80,8 +80,12 @@ export default defineNuxtConfig({
           sizes: "180x180",
           href: "/apple-touch-icon-180x180.png",
         },
-        { rel: "mask-icon", href: "/maskable-icon-512x512.svg", color: "#FFFFFF" },
+        {
+          rel: "mask-icon",
+          href: "/maskable-icon-512x512.svg",
+          color: "#FFFFFF",
+        },
       ],
-    }
-  }
+    },
+  },
 });
